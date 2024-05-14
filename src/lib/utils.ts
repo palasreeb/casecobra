c83392982c8d6aa9c04b247e57f1262dafe4126e
palasreeb <palasree83@gmail.com>
2024-05-14 12:05:17 +0530

    initial commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,14 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const formatPrice = () => {
+  return
+}
+
+export const constructMetadata = () => {
+  return
+}
